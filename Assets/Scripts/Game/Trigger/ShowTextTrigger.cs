717554bdd1f08d8acc146bdecd31720759b48aaf
atil <hyperion91@gmail.com>
2020-12-10 20:15:58 +0100

    Final touches

--- Assets/Scripts/Game/Trigger/ShowTextTrigger.cs
+++ Assets/Scripts/Game/Trigger/ShowTextTrigger.cs
@@ -6,8 +6,23 @@ public class ShowTextTrigger : TriggerBase
     public string TextToShow;
     public float Duration = 3f;
 
+    public float Delay = -1f;
+
     protected override void OnTriggered()
     {
+        if (Delay > 0)
+        {
+            CoroutineStarter.Run(DelayedShowTextCoroutine());
+        }
+        else
+        {
+            FindObjectOfType<Ui>().ShowText(TextToShow, Duration);
+        }
+    }
+
+    private IEnumerator DelayedShowTextCoroutine()
+    {
+        yield return new WaitForSeconds(Delay);
         FindObjectOfType<Ui>().ShowText(TextToShow, Duration);
     }
 }
